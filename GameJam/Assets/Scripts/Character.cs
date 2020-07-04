3c81f597ca82ea93c6b13d8cb50695c6a883cb94
CorsiRiccardo <corsi.riccardo@hotmail.it>
2020-07-04 18:37:00 +0200

    movement

--- GameJam/Assets/Scripts/Character.cs
+++ GameJam/Assets/Scripts/Character.cs
@@ -1,8 +1,21 @@
-﻿using System.Collections;
-using DefaultNamespace;
+﻿using System;
+using System.Collections;
+using Rewired;
 
-public class Character : Entity
-{
+public class Character : Entity {
+    public Player player;
+    
+    public enum CharPhase {
+        One,
+        Two,
+        Three,
+    }
+
+    public CharPhase currentPhase = CharPhase.One;
+
+    private void Awake() {
+        player = ReInput.players.GetPlayer(0);
+    }
 
     private void AddComponents() {
         components.Add(typeof(CharacterMovement), GetComponent<CharacterMovement>());
--- GameJam/Assets/Scripts/CharacterMovement.cs
+++ GameJam/Assets/Scripts/CharacterMovement.cs
@@ -1,7 +1,71 @@
-namespace DefaultNamespace {
 
-	public class CharacterMovement : EntityComponent<Character> {
-		
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+public class CharacterMovement : EntityComponent<Character> {
+
+	private Rigidbody rb;
+	private Animator animator;
+	public float speed;
+	public float rotSpeed;
+
+	private bool isJumping;
+	private bool canJump;
+	public float jumpIntensity = 5;
+	public float raycastLenght= 1;
+	public LayerMask jumpDetection;
+	public bool IsJumping {
+		get => isJumping;
+		set {
+			isJumping = value;
+			animator.SetBool("isJumping",value);
+		}
 	}
 
+	protected override void Awake() {
+		base.Awake();
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void Update() {
+		ManageJump();
+	}
+
+	private void ManageJump() {
+
+		canJump = Physics.Raycast(transform.position, Vector3.down,raycastLenght,jumpDetection);
+		Debug.Log(canJump);
+
+		// if (Entity.currentPhase != Character.CharPhase.One) {
+			if (Entity.player.GetButtonDown("Jump")) {
+				if (canJump) {
+					IsJumping = true;
+					rb.AddForce(Vector3.up * jumpIntensity,ForceMode.Impulse);
+				}
+			}
+		// }
+	}
+
+	private void FixedUpdate() {
+		ManageMovement();
+		ManageRotation();
+	}
+
+	private void ManageMovement() {
+		var z = Entity.player.GetAxis("MoveV");
+		z = Mathf.Clamp(z, 0, 100000);
+		rb.velocity = transform.forward * (speed * z);
+	}
+
+	private void ManageRotation() {
+		var x = Entity.player.GetAxis("MoveH");
+		transform.Rotate(Vector3.up, x * rotSpeed);
+	}
+
+	private void OnDrawGizmosSelected() {
+		Gizmos.color= Color.magenta;
+		Gizmos.DrawRay(transform.position , Vector3.down);
+	}
 }
