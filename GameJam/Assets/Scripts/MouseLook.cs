e04cdc97907efd01017398404800115ce40ecfb3
CorsiRiccardo <corsi.riccardo@hotmail.it>
2020-07-05 15:35:05 +0200

    fps

---
+++ GameJam/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Rewired;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+    private Player player;
+    public float mouseSensitivity;
+
+    public Transform playerBody;
+    private float xRot=0;
+
+    private void Awake() {
+        player = ReInput.players.GetPlayer(0);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float mouseX = player.GetAxis("MouseH") * mouseSensitivity * Time.deltaTime;
+        float mouseY = player.GetAxis("MouseV") * mouseSensitivity * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90, 90);
+        
+        transform.localRotation=Quaternion.Euler(xRot,0,0);
+        playerBody.transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ GameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Rewired;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Player player;
+    private CharacterController controller;
+
+    public float speed = 10;
+
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    private bool isGrounded;
+    private Vector3 vel;
+    public float jumpH = 3f;
+    
+    private void Awake() {
+        player = ReInput.players.GetPlayer(0);
+        controller = GetComponent<CharacterController>();
+    }
+    
+    // Update is called once per frame
+    void Update() {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.4f, groundMask);
+
+        if (isGrounded && vel.y < 0) {
+            vel.y = -2f;
+        }
+        float x = player.GetAxis("MoveH");
+        float z = player.GetAxis("MoveV");
+
+        Vector3 move = transform.right * x + transform.forward * z; 
+        controller.Move(move * (speed * Time.deltaTime));
+        
+        if (player.GetButtonDown("Jump") && isGrounded) {
+            vel.y = Mathf.Sqrt(jumpH * -2f * -9.81f);
+        }
+        vel.y -= 9.81f * Time.deltaTime;
+        controller.Move(vel * Time.deltaTime);
+
+    }
+}
